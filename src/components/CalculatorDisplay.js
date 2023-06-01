567a30fb28765ab8e474669a96ee711d07498e10
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-06-01 18:03:08 +0500

    Complete calculator functionality

--- src/components/CalculatorDisplay.js
+++ src/components/CalculatorDisplay.js
@@ -1,7 +1,25 @@
-function CalculatorDisplay() {
+import PropTypes from 'prop-types';
+
+function CalculatorDisplay({ total, operation, next }) {
   return (
-    <div className="display-screen">0</div>
+    <div className="display-screen">
+      {total !== 0 ? total : ' '}
+      {operation}
+      {next}
+    </div>
   );
 }
+
+CalculatorDisplay.propTypes = {
+  total: PropTypes.number,
+  operation: PropTypes.string,
+  next: PropTypes.string,
+};
+
+CalculatorDisplay.defaultProps = {
+  total: null,
+  operation: null,
+  next: null,
+};
 
 export default CalculatorDisplay;
--- src/components/CalculatorKeys.js
+++ src/components/CalculatorKeys.js
@@ -1,28 +1,41 @@
+import React, { useState } from 'react';
 import CalculatorDisplay from './CalculatorDisplay';
+import calculate from '../logic/calculate';
 
 function CalculatorKeys() {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
+
+  const btnClick = (e) => {
+    const res = calculate({ total, next, operation }, e.target.innerText);
+    setTotal(res.total);
+    setNext(res.next);
+    setOperation(res.operation);
+  };
+
   return (
     <div className="wrapper">
-      <CalculatorDisplay />
-      <div className="btn light-gray">AC</div>
-      <div className="btn light-gray">+/-</div>
-      <div className="btn light-gray">%</div>
-      <div className="btn orange">÷</div>
-      <div className="btn light-gray">7</div>
-      <div className="btn light-gray">8</div>
-      <div className="btn light-gray">9</div>
-      <div className="btn orange">X</div>
-      <div className="btn light-gray">4</div>
-      <div className="btn light-gray">5</div>
-      <div className="btn light-gray">6</div>
-      <div className="btn orange">+</div>
-      <div className="btn light-gray">1</div>
-      <div className="btn light-gray">2</div>
-      <div className="btn light-gray">3</div>
-      <div className="btn orange">-</div>
-      <div className="btn btn-0 light-gray">0</div>
-      <div className="btn light-gray">.</div>
-      <div className="btn orange">=</div>
+      <CalculatorDisplay total={Number(total)} operation={operation} next={next} />
+      <button type="button" className="btn light-gray" onClick={btnClick}>AC</button>
+      <button type="button" className="btn light-gray" onClick={btnClick}>+/-</button>
+      <button type="button" className="btn light-gray" onClick={btnClick}>%</button>
+      <button type="button" className="btn orange" onClick={btnClick}>÷</button>
+      <button type="button" className="btn light-gray" onClick={btnClick}>7</button>
+      <button type="button" className="btn light-gray" onClick={btnClick}>8</button>
+      <button type="button" className="btn light-gray" onClick={btnClick}>9</button>
+      <button type="button" className="btn orange" onClick={btnClick}>X</button>
+      <button type="button" className="btn light-gray" onClick={btnClick}>4</button>
+      <button type="button" className="btn light-gray" onClick={btnClick}>5</button>
+      <button type="button" className="btn light-gray" onClick={btnClick}>6</button>
+      <button type="button" className="btn orange" onClick={btnClick}>+</button>
+      <button type="button" className="btn light-gray" onClick={btnClick}>1</button>
+      <button type="button" className="btn light-gray" onClick={btnClick}>2</button>
+      <button type="button" className="btn light-gray" onClick={btnClick}>3</button>
+      <button type="button" className="btn orange" onClick={btnClick}>-</button>
+      <button type="button" className="btn btn-0 light-gray" onClick={btnClick}>0</button>
+      <button type="button" className="btn light-gray" onClick={btnClick}>.</button>
+      <button type="button" className="btn orange" onClick={btnClick}>=</button>
     </div>
   );
 }
